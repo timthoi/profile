910f58389a451536f0c4a9cccaed1a229618869e
timthoi <idndhoang@gmail.com>
2023-05-11 11:32:01 +0700

    init

---
+++ site.config.ts
@@ -0,0 +1,65 @@
+import { siteConfig } from './lib/site-config'
+
+export default siteConfig({
+  // the site's root Notion page (required)
+  rootNotionPageId: 'f67e87f4e7ac455d8ac2d251a0c9f2d1',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'Next.js Notion Starter Kit',
+  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  author: 'timthoi',
+
+  // open graph metadata (optional)
+  description: 'Example Next.js Notion Starter Kit Site',
+
+  // social usernames (optional)
+  twitter: '',
+  github: 'timthoi',
+  linkedin: 'harvey-nguyen-295060141',
+  // mastodon: '#', // optional mastodon profile URL, provides link verification
+  // newsletter: '#', // optional newsletter URL
+  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  isPreviewImageSupportEnabled: true,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null,
+
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages
+  //navigationStyle: 'default',
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About me',
+      pageId: '96c106be87144e6dae4b21abe8dd859b'
+    },
+    {
+      title: 'Contact',
+      pageId: 'ce6074f5961f4aeda655a5c30a23539f'
+    }
+  ]
+})
